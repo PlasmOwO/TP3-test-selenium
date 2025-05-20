11cc93ac446ee66e4970add1bda40cc8e6dc900d
PlasmOwO <thomas.plasmo@gmail.com>
2025-05-20 15:19:51 +0200

    🧪 Tests pytest with selenium

--- tp3_tests_selenium/tests/test_login.py
+++ tp3_tests_selenium/tests/test_login.py
@@ -13,13 +13,32 @@ def driver():
     driver.quit()
 
 def test_login_success(driver):
-    # TODO
-    pass
+    username_form = driver.find_element(By.NAME, "username")
+    password_form = driver.find_element(By.NAME, "password")
+    login_button = driver.find_element(By.XPATH, "/html/body/form/button[@type='submit']")
+
+    username_form.send_keys("admin")
+    password_form.send_keys("password123")
+    login_button.click()
+    assert driver.current_url == "http://localhost:5000/dashboard"
 
 def test_login_failure_wrong_password(driver):
-    # TODO
-    pass
+    username_form = driver.find_element(By.NAME, "username")
+    password_form = driver.find_element(By.NAME, "password")
+    login_button = driver.find_element(By.XPATH, "/html/body/form/button[@type='submit']")
+
+    username_form.send_keys("admin")
+    password_form.send_keys("wrongpassword")
+    login_button.click()
+    assert driver.find_element(By.XPATH, "/html/body/p").text == "Identifiants incorrects."
 
 def test_login_empty_fields(driver):
-    # TODO
-    pass
+    username_form = driver.find_element(By.NAME, "username")
+    password_form = driver.find_element(By.NAME, "password")
+    login_button = driver.find_element(By.XPATH, "/html/body/form/button[@type='submit']")
+
+    username_form.send_keys("")
+    password_form.send_keys("")
+    login_button.click()
+
+    assert driver.find_element(By.XPATH, "/html/body/p").text == "Tous les champs sont requis."
